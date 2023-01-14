46ee7fcd175220ddf0a5481941634c2a910af705
Valiyeva Elmira <valiyeva1995elya@gmail.com>
2023-01-14 16:33:15 +0600

    front style

--- jse-mono-front/script.js
+++ jse-mono-front/script.js
@@ -26,13 +26,16 @@ const postData = async (route, payload) => {
 
 const drawCars = async () => {
     const cars = await fetchData("/cars");
+    const users = await fetchData("/users");
 
     carsContainer.innerHTML = "";
+    usersContainer.innerHTML = "";
 
 
     for (const car of cars) {
         carsContainer.innerHTML += `
             <div class="car_item">
+                <p>ID: ${car._id}</p>
                 <p>Model: ${car.model}</p>
                 <p>Color: ${car.color}</p>
                 <p>Year: ${car.year}</p>
@@ -41,10 +44,46 @@ const drawCars = async () => {
                 ${car.ownersHistory.map(item => `<li> ${item?.fullName}</li>`).join("")}
         
             </div>
-            <hr>
+               
+        `
+
+    }
+
+
+    for (const user of users) {
+        usersContainer.innerHTML += `
+            <div class="user_item">
+                <p>ID: ${user._id}</p>
+                <p>FullName: ${user.fullName}</p>
+                <p>Login: ${user.login}</p>
+                <p>Password: ${user.password}</p>
+        
+            </div>
                
         `
 
     }
 };
 drawCars()
+
+change_owner.addEventListener("click", () => {
+    const newProductsName = document.querySelector("#new_products_name").value;
+    const newProductsPrice = document.querySelector("#new_products_price").value;
+    const checkboxCategory = document.querySelector(".checkbox");
+    
+    const payload = {
+        name: newProductsName,
+        price: newProductsPrice,
+        categoryId: checkboxCategory.textContent,
+    };
+    fetch(BASE_URL + "/products", {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "post",
+        body: JSON.stringify(payload)
+    })
+        .then(() => loadData())
+        .catch(() => alert("products create error"));
+})
